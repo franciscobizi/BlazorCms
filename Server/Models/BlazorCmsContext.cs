821c2de52a963d4da6aae002c00592b912405f7e
Francisco Bizi <francisco.bizi@avantime.se>
2021-01-29 01:36:10 +0200

    Changed connection string

--- Server/Models/BlazorCmsContext.cs
+++ Server/Models/BlazorCmsContext.cs
@@ -24,12 +24,14 @@ namespace BlazorCms.Server.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlite("Name=BlazorCms");
+                optionsBuilder.UseSqlServer("Name=BlazorCms");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
             modelBuilder.Entity<Post>(entity =>
             {
                 entity.ToTable("Post");
@@ -38,28 +40,39 @@ namespace BlazorCms.Server.Models
 
                 entity.Property(e => e.PostAuthor).HasColumnName("post_author");
 
-                entity.Property(e => e.PostCategory).HasColumnName("post_category");
+                entity.Property(e => e.PostCategory)
+                    .HasMaxLength(200)
+                    .HasColumnName("post_category");
 
-                entity.Property(e => e.PostContent).HasColumnName("post_content");
+                entity.Property(e => e.PostContent)
+                    .HasColumnType("text")
+                    .HasColumnName("post_content");
 
-                entity.Property(e => e.PostCreated).HasColumnName("post_created");
+                entity.Property(e => e.PostCreated)
+                    .HasMaxLength(50)
+                    .HasColumnName("post_created");
 
                 entity.Property(e => e.PostPermalink)
-                    .IsRequired()
+                    .HasMaxLength(300)
                     .HasColumnName("post_permalink");
 
-                entity.Property(e => e.PostThumbnail).HasColumnName("post_thumbnail");
+                entity.Property(e => e.PostThumbnail)
+                    .HasColumnType("text")
+                    .HasColumnName("post_thumbnail");
 
                 entity.Property(e => e.PostTitle)
                     .IsRequired()
+                    .HasMaxLength(255)
                     .HasColumnName("post_title");
 
-                entity.Property(e => e.PostUpdated).HasColumnName("post_updated");
+                entity.Property(e => e.PostUpdated)
+                    .HasMaxLength(50)
+                    .HasColumnName("post_updated");
 
                 entity.HasOne(d => d.PostAuthorNavigation)
                     .WithMany(p => p.Posts)
                     .HasForeignKey(d => d.PostAuthor)
-                    .OnDelete(DeleteBehavior.ClientSetNull);
+                    .HasConstraintName("FK__Post__post_autho__5EBF139D");
             });
 
             modelBuilder.Entity<User>(entity =>
@@ -68,27 +81,42 @@ namespace BlazorCms.Server.Models
 
                 entity.Property(e => e.UserId).HasColumnName("user_id");
 
-                entity.Property(e => e.UserAvatar).HasColumnName("user_avatar");
+                entity.Property(e => e.UserAvatar)
+                    .HasMaxLength(100)
+                    .HasColumnName("user_avatar");
 
                 entity.Property(e => e.UserEmail)
                     .IsRequired()
+                    .HasMaxLength(255)
                     .HasColumnName("user_email");
 
-                entity.Property(e => e.UserFname).HasColumnName("user_fname");
+                entity.Property(e => e.UserFname)
+                    .HasMaxLength(100)
+                    .HasColumnName("user_fname");
 
-                entity.Property(e => e.UserLname).HasColumnName("user_lname");
+                entity.Property(e => e.UserLname)
+                    .HasMaxLength(100)
+                    .HasColumnName("user_lname");
 
-                entity.Property(e => e.UserLogged).HasColumnName("user_logged");
+                entity.Property(e => e.UserLogged)
+                    .HasMaxLength(50)
+                    .HasColumnName("user_logged");
 
                 entity.Property(e => e.UserPass)
-                    .IsRequired()
+                    .HasMaxLength(300)
                     .HasColumnName("user_pass");
 
-                entity.Property(e => e.UserRegistered).HasColumnName("user_registered");
+                entity.Property(e => e.UserRegistered)
+                    .HasMaxLength(50)
+                    .HasColumnName("user_registered");
 
-                entity.Property(e => e.UserRoles).HasColumnName("user_roles");
+                entity.Property(e => e.UserRoles)
+                    .HasMaxLength(50)
+                    .HasColumnName("user_roles");
 
-                entity.Property(e => e.UserSource).HasColumnName("user_source");
+                entity.Property(e => e.UserSource)
+                    .HasMaxLength(50)
+                    .HasColumnName("user_source");
             });
 
             OnModelCreatingPartial(modelBuilder);
