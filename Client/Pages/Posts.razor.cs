55fd8cfcfee6cd8fff662f464e0739b632095d4d
Francisco Bizi <francisco.bizi@avantime.se>
2021-03-11 13:04:21 +0200

    Added code behind for posts page

---
+++ Client/Pages/Posts.razor.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using BlazorCms.ViewModels;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+//using BlazorCms.Shared.Mapping;
+
+namespace BlazorCms.Client.Pages
+{
+    public partial class Posts
+    {
+        
+        [Inject] IPostViewModel _postViewModel { get; set; }
+        private readonly NavigationManager _navigate;
+        //protected List<PostResponse> postResponse = new List<PostResponse>();
+
+        protected override async Task OnInitializedAsync() => await _postViewModel.GetAll();
+        
+    }
+}
